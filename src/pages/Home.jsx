025b22d4aa311d2ffe294eefe75852e71bb35c62
George Ofogba <ofogbageorge3@gmail.com>
2024-09-30 20:27:11 +0200

    home page added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import SpecialityMenu from '../components/SpecialityMenu';
+import Header from '../components/Header';
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <SpecialityMenu />
+    </div>
+  );
+};
+
+export default Home;
